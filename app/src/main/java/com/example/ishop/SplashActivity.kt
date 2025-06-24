ecfbdb7ca6c23b40e052c7f4d8f3f00baf717f7f
hollali <dheztinykartel@gmail.com>
2025-06-24 21:14:06 +0000

    all

--- app/src/main/java/com/example/ishop/SplashActivity.kt
+++ app/src/main/java/com/example/ishop/SplashActivity.kt
@@ -14,20 +14,20 @@ class SplashActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //*---making layout fullscreen--
-        //*---hiding the title bar
+        //*---making layout fullscreen--//
+        //*---hiding the title bar--//
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
 
         //*--hiding the status bar
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         //*--- Setting content view
         setContentView(R.layout.splash_activity)
 
-        //*---Creating layout instance with animation---
+        //*---Creating layout instance with animation---//
         val backgroundImage : ImageView = findViewById(R.id.write)
         val backgroundLabel : TextView = findViewById(R.id.ipaper)
 
